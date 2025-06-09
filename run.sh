82d78a4bd691f1ee53198a257ce87e0519ed9970
Nils <nils.bjorklin@gmail.com>
2025-06-09 16:58:58 +0200

    Updated config

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z "${OLLAMA_MODEL}" ]; then
+  echo "OLLAMA_MODEL environment variable is not set."
+  exit 1
+else
+  OLLAMA_MODEL="${OLLAMA_MODEL}"
+  echo "Using OLLAMA_MODEL: ${OLLAMA_MODEL}"
+fi
+
 echo "Starting Ollama server..."
 ollama serve &
 
@@ -10,3 +18,5 @@ until curl -s http://localhost:11434 > /dev/null; do
 done
 
 echo "Ollama is running"
+
+ollama run $OLLAMA_MODEL
